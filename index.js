1a66880a9c3dad853ce657ec9653cfc1514827a9
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-01 20:19:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ $(function () {
         })
         names = names.slice(0, -1);
       
-        if (result < ２５0){
+        if (result < 250){
             var msg = `【現在のガス料金情報】AAAランクです\n【内訳】\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${result}`;
 //            var msg2 = `AAAランク`; 
         } else {
