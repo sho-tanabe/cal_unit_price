fa9d56b0e94662dbe237b3e0d682999287c2fb60
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-02 00:49:17 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -24,7 +24,8 @@ function initializeLiff(liffId) {
 
 function sendText(text) {
     if (!liff.isInClient()) {
-        shareTargetPicker(text);
+        alert('アラートのメッセージ');
+//        shareTargetPicker(text);
 //WEBブラウザアクセスの場合の動作↓
 //        liff.openWindow({
 //            url: "https://goofy-offer-5121.glide.page/dl//s/d0a5f4",
