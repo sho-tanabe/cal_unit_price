a453207bdda04a64d9c7b92a2f05674a61c6a35f
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-02 11:49:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -80,25 +80,25 @@ $(function () {
 //----------------------------------------以下住所検索テスト----------------------------------------
 
 
-    $('#searchbtn').on('click', () => {
-        $.ajax({
-            url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#postcode').val(),
-            dataType: 'jsonp',
-        }).done((data) => {
-            if (data.results) { 
-                getData(data.results[0]);
-            } else {
-                alert('該当データが見つかりません');
-            }
-        }).fail((data) => {
-            alert('通信に失敗しました');
-        });
-    });
+//    $('#searchbtn').on('click', () => {
+//        $.ajax({
+//            url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#postcode').val(),
+//            dataType: 'jsonp',
+//        }).done((data) => {
+//            if (data.results) { 
+//                getData(data.results[0]);
+//            } else {
+//                alert('該当データが見つかりません');
+//            }
+//        }).fail((data) => {
+//            alert('通信に失敗しました');
+//        });
+//    });
 
-    function getData(data) {
-        $('#pref').val(data.address1);
-        $('#city').val(data.address2);
-        $('#address').val(data.address3);
+//    function getData(data) {
+//        $('#pref').val(data.address1);
+//        $('#city').val(data.address2);
+//        $('#address').val(data.address3);
     }
 
 //----------------------------------------以上住所検索テスト----------------------------------------
