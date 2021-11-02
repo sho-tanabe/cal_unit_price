85ceb65a75f9c4abdbcfb0d563944a5f6d53bad0
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-02 12:49:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -27,19 +27,17 @@ $(function () {
         var text10 = $('input[name="text10"]').val();
         var date = $('input[name="date"]').val();
         var number = $('input[name="number"]:checked').val();
-
-//        var address = $('input[name="address"]').val();
-//        var address = `住所情報貼り付けテスト中`;
+        
+        //        var address = $('input[name="address"]').val();
+        var address = `住所情報貼り付けテスト中`;
         
         var names = '';
         $('#form-name').children().each(function (i, elm) {
             names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
-        
         //練習エリア終了
-
-
+        
         //計算要素項目
         var billingamount = $('input[name="billingamount"]').val();
         var basiccharge = $('input[name="basiccharge"]').val();
@@ -62,9 +60,9 @@ $(function () {
             } else {
                 var costcutfloor = Math.floor(costcut / 1000) * 1000;
                 var costcutmsg = `約${costcutfloor}円程度`;
-                }                
+                }
         
-        var msg = `【現在のガス料金情報】\nお住まいの地域:\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n${costcutmsg}`;
+        var msg = `【現在のガス料金情報】\nお住まいの地域:${address}\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n${costcutmsg}`;
         
         if (difference < 280){
             var msg2 = `Sランク`; 
@@ -104,7 +102,7 @@ $(function () {
 //        $('#pref').val(data.address1);
 //        $('#city').val(data.address2);
 //        $('#address').val(data.address3);
-    }
+//    }
 
 //----------------------------------------以上住所検索テスト----------------------------------------
 
