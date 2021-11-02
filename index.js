f9b4a2ed69874a4a0684efa664708b29b2ff84f5
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-02 16:21:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -63,15 +63,13 @@ $(function () {
                 var costcutmsg = `約${costcutfloor}円程度`;
                 }
         
-        var msg = `【現在お住まいの地域】\n${address1}\n${address2}\n【現在のガス料金情報】\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n${costcutmsg}`;
+//        var msg = `【現在お住まいの地域】\n${address1}\n${address2}\n【現在のガス料金情報】\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n${costcutmsg}`;
 
-//        if ( 7 >= address1num || address1num >= 15 ){
-//            var msg2 = `対象外地域`;
-//        } else if (difference < 280){
-//            var msg2 = `Sランク`; 
-//        } else {
-//            var msg2 = `Bランク`; 
-//        }
+        if ( 7 >= address1num || address1num >= 15 ){
+            var msg = `【現在お住まいの地域】\n${address1}\n${address2}\n【現在のガス料金情報】\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n対象外地域のため判定できません。`;
+        } else {
+            var msg = `【現在お住まいの地域】\n${address1}\n${address2}\n【現在のガス料金情報】\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n${costcutmsg}`;
+        }
 
                 
         if ( 7 >= address1num || address1num >= 15 ){
