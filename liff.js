f8ede6065cda6d846467b8f141dafe77cc4c8324
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-03 02:51:22 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -29,7 +29,7 @@ function sendText(text) {
 //WEBブラウザアクセス以外の場合の動作↑
     } else {
         sendMessages(text);
-//        sendautomail(text);
+//        sendautomail(text);//これを利用すると画面が3回目以降フリーズする現象に見舞われる。
 //        sendLineNotifyMessage();//実験中。
 //        myFunction();//実験中２。
     }
