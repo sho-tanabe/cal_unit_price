3a98d6d47f3d85e05eeff5ba507489390a8f2e8d
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-02 10:56:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -49,16 +49,16 @@ $(function () {
 
         //コスト削減予定額（LINEトーク送信用）
         if (costcut < 1000){
-            var costcutround = `判定結果をご確認ください(${costcut})`;
+            var costcutmsg = `判定結果をご確認ください(${costcut})`;
         }　else if (costcut < 10000){
             var costcutfloor = Math.floor(costcut / 100) * 100;
-            var costcutround = `約${costcutfloor}円程度`;
+            var costcutmsg = `約${costcutfloor}円程度`;
             } else {
                 var costcutfloor = Math.floor(costcut / 1000) * 1000;
-                var costcutround = `約${costcutfloor}円程度`;
+                var costcutmsg = `約${costcutfloor}円程度`;
                 }                
         
-        var msg = `【現在のガス料金情報】\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n${costcut}\n${costcutround}`;
+        var msg = `【現在のガス料金情報】\nご請求予定金額(円):${billingamount}\n基本料金(円):${basiccharge}\n今回ご使用量(㎥):${quantity}\nガス料金単価:${resultround}\n-----------\n【お安くなる金額目安】\n${costcutmsg}`;
         
         if (result < 280){
             var msg2 = `Sランク`; 
