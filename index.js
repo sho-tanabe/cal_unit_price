1c059c01c854986be0ef68cb88c30d5fe96fa020
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-03 22:12:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,25 @@ $(function () {
         }
     });
 
+    
+    //ブラウザバリデーション
+
+    (function () {
+        var forms = document.querySelectorAll('.needs-validation');
+        Array.prototype.slice.call(forms).forEach(function (form) {    
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();        
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        })
+    })();
+    
+    
+    
+    
     // 送信
     $('form').submit(function () {
 
